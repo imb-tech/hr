cd15d4defa4c98031d4fc11ca358ef0075bd82ce
Doniyor Eshmamatov <edoniyorbekdev@gmail.com>
2025-05-10 15:38:48 +0500

    d

--- src/routes/_main.tsx
+++ src/routes/_main.tsx
@@ -1,8 +1,15 @@
 import DefaultLayout from "@/layouts/default";
 import { createFileRoute, Outlet } from "@tanstack/react-router";
 
+
 export const Route = createFileRoute("/_main")({
   component: RouteComponent,
+  validateSearch: (search: SearchParams): SearchParams => {
+    return {
+      page: search?.page ?? undefined,
+      filter: search.filter ?? undefined,
+    };
+  },
 });
 
 function RouteComponent() {
